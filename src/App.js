c698915a184f26fad8fd78964b53025a2eaa47fb
shoneriki <ikirenohs@gmail.com>
2021-07-18 20:03:37 +0900

    select option updates when different select is clicked

--- src/App.js
+++ src/App.js
@@ -8,13 +8,18 @@ const BASE_URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=bc0919d0fe
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([])
-  console.log(currencyOptions)
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+
 
   useEffect(() => {
     fetch(BASE_URL)
       .then(res => res.json())
       .then(data => {
+      const firstCurrency = Object.keys(data.rates)[0]
         setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        setToCurrency(firstCurrency)
       })
   }, [])
 
@@ -23,10 +28,14 @@ function App() {
       <h1>Convert</h1>
       <CurrencyRow
         currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={e => setFromCurrency(e.target.value)}
       />
       <div className="equals">=</div>
       <CurrencyRow
         currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={e => setToCurrency(e.target.value)}
       />
     </>
   );
